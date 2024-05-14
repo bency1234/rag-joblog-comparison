aa2cfcd9b00dec0f43577e796c785a8e143f3169
bency1234 <andreabency@bitcot.com>
2024-05-14 14:32:47 +0530

    Reformat code

--- apis/scripts/automation.py
+++ apis/scripts/automation.py
@@ -6,6 +6,7 @@ import time
 import pandas as pd
 import xlsxwriter
 from common.envs import logger
+from scripts.constants import MAX_REQUESTS_PER_MINUTE, TIME_INTERVAL
 from scripts.css_selectors import RESPONSE_CONTAINER
 from scripts.utils import fill_user_info, visit_the_website
 from selenium import webdriver
@@ -34,7 +35,7 @@ timestamp_str = current_datetime.strftime("%Y%m%d%H%M%S")
 INPUT_SHEET_PATH = os.getenv("INPUT_SHEET_PATH")
 URL = os.getenv("URL")
 file_name_without_extension = os.path.splitext(os.path.basename(INPUT_SHEET_PATH))[0]
-OUTPUT_SHEET_PATH = f"va_mtp_{file_name_without_extension}_{timestamp_str}.xlsx"
+OUTPUT_SHEET_PATH = f"joblog_{file_name_without_extension}_{timestamp_str}.xlsx"
 
 LOGGED_IN = False
 
@@ -48,48 +49,10 @@ wait = WebDriverWait(driver, WAIT_TIMEOUT)
 driver.get(URL)
 
 
-def check_element_length(css_selector, length):
-    selector = (By.CSS_SELECTOR, css_selector)
-    elements = wait.until(EC.presence_of_all_elements_located(selector))
-    while len(elements) != length:
-        time.sleep(10)
-        elements = driver.find_elements(*selector)
-
-
-combined_df = pd.DataFrame()
-
-
-xls = pd.ExcelFile(INPUT_SHEET_PATH)
-
-
-# Create a dictionary to store DataFrames for each sheet
-data_dict = {}
-
-
-for sheet_name in xls.sheet_names:
-    df = pd.read_excel(xls, sheet_name=sheet_name)
-
-    visit_the_website(HOST, driver)
-    if not LOGGED_IN:
-        fill_user_info(wait)
-        LOGGED_IN = True
-
-    driver.implicitly_wait(20)
+def check_current_time(requests_in_current_minute, data_dict, start_time):
+    records = df
     combined_df = pd.DataFrame(columns=SHEET_COLUMNS)
 
-    # Set the maximum number of requests per minute and the time interval
-    MAX_REQUESTS_PER_MINUTE = 5
-    TIME_INTERVAL = 60  # seconds
-
-    # Initialize a counter to keep track of requests in the current minute
-    requests_in_current_minute = 0
-
-    # Initialize the start_time variable to track the current minute
-    start_time = time.time()
-
-    skipped_rows = 0
-
-    records = df
     with tqdm(total=len(records)) as pbar:
         for index, row in df.iterrows():
             time.sleep(5)
@@ -110,68 +73,107 @@ for sheet_name in xls.sheet_names:
                 # Reset the request counter for the new minute
                 requests_in_current_minute = 0
 
-            user_input = row["user_input"]
-
-            if len(user_input) < 500:
-                try:
-                    input_element = wait.until(
-                        EC.element_to_be_clickable((By.CSS_SELECTOR, "#myTextArea"))
-                    )
-                    input_element.clear()
-
-                    logger.info(f"Trying this question - {user_input}")
-
-                    lines = re.split(r"[\n\t]", user_input)
-
-                    # Iterate through each line and send it to the text area
-                    for line in lines:
-                        input_element.send_keys(line)
-                        input_element.send_keys(Keys.SHIFT + Keys.ENTER)
-
-                    input_element.send_keys(Keys.RETURN)
-
-                    # Find the element with a class containing "bot_span"
-                    check_element_length(RESPONSE_CONTAINER, index + 1 - skipped_rows)
-                    all_paragraphs = driver.find_elements(
-                        By.CSS_SELECTOR, RESPONSE_CONTAINER
-                    )
-                    response = all_paragraphs[-1]
-
-                    if "Sorry" in response.text:
-                        time.sleep(5)
-
-                    response_df = pd.DataFrame(
-                        {
-                            SHEET_COLUMNS[0]: [user_input],
-                            SHEET_COLUMNS[1]: [response.text],
-                            SHEET_COLUMNS[2]: [response.get_attribute("innerHTML")],
-                        }
-                    )
-                    combined_df = pd.concat(
-                        [combined_df, response_df], ignore_index=True
-                    )
-                except NoSuchElementException as e:
-                    # Handle the case where the input element is not found
-                    logger.info(f"Element not found for index {index}: {e}")
-            else:
-                skipped_rows += 1
-                response = "Question is too long"
-                response_df = pd.DataFrame(
-                    {
-                        SHEET_COLUMNS[0]: [user_input],
-                        SHEET_COLUMNS[1]: [response],
-                        SHEET_COLUMNS[2]: [response],
-                    }
-                )
+            # To pass the user input to the bot and get the response
+            response_df = user_input(row, index, skipped_rows)
+            if response_df is not None:
                 combined_df = pd.concat([combined_df, response_df], ignore_index=True)
-
             # Increment the request counter for the current minute
             requests_in_current_minute += 1
 
             pbar.update(1)
 
-        # Store the DataFrame for the current sheet in the dictionary
-        data_dict[sheet_name] = combined_df
+    # Store the DataFrame for the current sheet in the dictionary
+    data_dict[sheet_name] = combined_df
+    return data_dict
+
+
+def user_input(row, index, skipped_rows):
+    user_input = row["user_input"]
+
+    if len(user_input) < 500:
+        try:
+            input_element = wait.until(
+                EC.element_to_be_clickable((By.CSS_SELECTOR, "#myTextArea"))
+            )
+            input_element.clear()
+
+            logger.info(f"Trying this question - {user_input}")
+
+            lines = re.split(r"[\n\t]", user_input)
+
+            # Iterate through each line and send it to the text area
+            for line in lines:
+                input_element.send_keys(line)
+                input_element.send_keys(Keys.SHIFT + Keys.ENTER)
+
+            input_element.send_keys(Keys.RETURN)
+
+            # Find the element with a class containing "bot_span"
+            check_element_length(RESPONSE_CONTAINER, index + 1 - skipped_rows)
+            all_paragraphs = driver.find_elements(By.CSS_SELECTOR, RESPONSE_CONTAINER)
+            response = all_paragraphs[-1]
+
+            if "Sorry" in response.text:
+                time.sleep(5)
+
+            response_df = pd.DataFrame(
+                {
+                    SHEET_COLUMNS[0]: [user_input],
+                    SHEET_COLUMNS[1]: [response.text],
+                    SHEET_COLUMNS[2]: [response.get_attribute("innerHTML")],
+                }
+            )
+            return response_df
+        except NoSuchElementException as e:
+            # Handle the case where the input element is not found
+            logger.info(f"Element not found for index {index}: {e}")
+    else:
+        skipped_rows += 1
+        response = "Question is too long"
+        response_df = pd.DataFrame(
+            {
+                SHEET_COLUMNS[0]: [user_input],
+                SHEET_COLUMNS[1]: [response],
+                SHEET_COLUMNS[2]: [response],
+            }
+        )
+        return response_df
+
+
+def check_element_length(css_selector, length):
+    selector = (By.CSS_SELECTOR, css_selector)
+    elements = wait.until(EC.presence_of_all_elements_located(selector))
+    while len(elements) != length:
+        time.sleep(10)
+        elements = driver.find_elements(*selector)
+
+
+combined_df = pd.DataFrame()
+
+xls = pd.ExcelFile(INPUT_SHEET_PATH)
+
+
+# Create a dictionary to store DataFrames for each sheet
+data_dict = {}
+
+
+for sheet_name in xls.sheet_names:
+    df = pd.read_excel(xls, sheet_name=sheet_name)
+
+    visit_the_website(HOST, driver)
+    if not LOGGED_IN:
+        fill_user_info(wait)
+        LOGGED_IN = True
+
+    # Initialize a counter to keep track of requests in the current minute
+    requests_in_current_minute = 0
+
+    # Initialize the start_time variable to track the current minute
+    start_time = time.time()
+
+    skipped_rows = 0
+
+    data_dict = check_current_time(requests_in_current_minute, data_dict, start_time)
 
 # Create an Excel writer
 writer = pd.ExcelWriter(OUTPUT_SHEET_PATH, engine="xlsxwriter")
