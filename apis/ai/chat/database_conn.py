a8845e347d399a3cbe2673b854e1e46a8ea3af13
bency1234 <andreabency@bitcot.com>
2024-06-05 14:47:13 +0530

    run isort

--- apis/ai/chat/database_conn.py
+++ apis/ai/chat/database_conn.py
@@ -3,8 +3,11 @@ import os
 import psycopg2
 from ai.chat.apis import call_add_error_details_api
 from ai.chat.error_details_exception import capture_error_details
+from common.app_utils import get_app
+from common.db import db
 from common.envs import get_secret_value_from_secret_manager, logger
 
+app = get_app(db)
 conn = psycopg2.connect(
     host=os.environ.get(
         "PGVECTOR_HOST", get_secret_value_from_secret_manager("DATABASE_HOST")
@@ -22,15 +25,18 @@ conn = psycopg2.connect(
 
 
 def is_collection_name_exists(collection_name_check):
-    try:
-        # Connect to the PostgreSQL database
-        query = "SELECT EXISTS (SELECT 1 FROM langchain_pg_collection WHERE name = %s)"
-        with conn.cursor() as cursor:
-            cursor.execute(query, (collection_name_check,))
-            exists = cursor.fetchone()[0]
-            return exists
+    with app.app_context():
+        try:
+            # Connect to the PostgreSQL database
+            query = (
+                "SELECT EXISTS (SELECT 1 FROM langchain_pg_collection WHERE name = %s)"
+            )
+            with conn.cursor() as cursor:
+                cursor.execute(query, (collection_name_check,))
+                exists = cursor.fetchone()[0]
+                return exists
 
-    except Exception as e:
-        error_info = capture_error_details(e)
-        call_add_error_details_api(user_input=None, error=error_info)
-        logger.info(f"An error occurred: {e}")
+        except Exception as e:
+            error_info = capture_error_details(e)
+            call_add_error_details_api(user_input=None, error=error_info)
+            logger.info(f"An error occurred: {e}")
